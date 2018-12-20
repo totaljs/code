f189e03e2a584484351a48dc6de2588b06e56af7
Peter Širka <petersirka@gmail.com>
2018-12-20 10:40:05 +0100

    Fixed `initials`.

--- schemas/users.js
+++ schemas/users.js
@@ -41,7 +41,7 @@ NEWSCHEMA('Users', function(schema) {
 		var model = $.model.$clean();
 		var tmp = model.name.split(' ');
 
-		model.initials = tmp[0][0] + tmp[1][0];
+		model.initials = tmp[0][0] + (tmp.length > 1 ? tmp[1][0] : '');
 
 		var item = MAIN.users.findItem('id', model.id);
 		if (item == null) {
