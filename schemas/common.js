97c24b99f1a318fe67c6e58811d0464380f9849b
Peter Širka <petersirka@gmail.com>
2019-02-12 15:54:07 +0100

    Added support for encoding/decoding `Hex`.

--- schemas/common.js
+++ schemas/common.js
@@ -22,13 +22,19 @@ NEWSCHEMA('Minify', function(schema) {
 
 NEWSCHEMA('Encoder', function(schema) {
 
-	schema.define('type', ['crc32', 'crc32unsigned', 'md5', 'sha1', 'sha256', 'sha512'], true);
+	schema.define('type', ['crc32', 'crc32unsigned', 'md5', 'sha1', 'sha256', 'sha512', 'hexe', 'hexd'], true);
 	schema.define('body', '[String]', true);
 
 	schema.addWorkflow('exec', function($) {
 		var model = $.model;
-		for (var i = 0; i < model.body.length; i++)
-			model.body[i] = model.body[i].hash(model.type).toString();
+		for (var i = 0; i < model.body.length; i++) {
+			if (model.type === 'hexe')
+				model.body[i] = U.createBuffer(model.body[i]).toString('hex');
+			else if (model.type === 'hexd')
+				model.body[i] = U.createBuffer(model.body[i], 'hex').toString('utf8');
+			else
+				model.body[i] = model.body[i].hash(model.type).toString();
+		}
 		$.callback(model.body);
 	});
 
