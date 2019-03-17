58d2805064d9727df4cb92a691d4a174cd4d7149
Peter Širka <petersirka@gmail.com>
2019-03-17 23:37:25 +0100

    Fixed auto-close-brackets.

--- public/js/editor.js
+++ public/js/editor.js
@@ -1192,7 +1192,7 @@ WAIT('CodeMirror.defineMode', function() {
 	mod(CodeMirror);
 })(function(CodeMirror) {
 
-	var reg_skip = (/[a-zA-Z'"`0-9/$-{@]/);
+	var reg_skip = (/[a-zA-Z'"`0-9/$\-{@]/);
 
 	var defaults = {
 		pairs: '()[]{}\'\'""',
