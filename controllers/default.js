721630daea980e33b0b581dfe5cc68e6e0f1a777
Peter Širka <petersirka@gmail.com>
2020-01-24 17:05:00 +0100

    Added an option for downloading documentation.

--- controllers/default.js
+++ controllers/default.js
@@ -1,9 +1,13 @@
+const Fs = require('fs');
+
 exports.install = function() {
 
-	ROUTE('/*', 'index', ['authorize']);
-	ROUTE('/*', 'login', ['unauthorize']);
-	ROUTE('/offline/');
-	ROUTE('/pause/');
+	ROUTE('+GET /*', 'index', ['authorize']);
+	ROUTE('-GET /*', 'login', ['unauthorize']);
+	ROUTE('GET  /offline/');
+	ROUTE('GET  /pause/');
+
+	ROUTE('+GET /docs/{id}/', docs);
 
 	// File routes
 	FILE('/manifest.json', manifest);
@@ -11,4 +15,30 @@ exports.install = function() {
 
 function manifest(req, res) {
 	res.content(200, '{"name":"{0}","short_name":"{0}","icons":[{"src":"/img/icon.png","sizes":"500x500","type":"image/png"}],"start_url":"/","display":"standalone"}'.format(CONF.name), U.getContentType('json'));
+}
+
+function docs(id) {
+
+	var self = this;
+	var project = MAIN.projects.findItem('id', id);
+	if (project == null) {
+		self.throw404();
+		return;
+	}
+
+	if (!self.user.sa) {
+		var user = project.users.indexOf($.user.id);
+		if (!user) {
+			self.throw401();
+			return;
+		}
+	}
+
+	NOSQL(id + '_parts').find().callback(function(err, response) {
+		var docs = JSON.stringify(response);
+		Fs.readFile(PATH.public('docs.html'), function(err, data) {
+			data = data.toString('utf8').replace(/NAME/g, project.name).replace(/DATA/, docs);
+			self.content(data, 'text/html');
+		});
+	});
 }
