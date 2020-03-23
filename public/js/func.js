d46179a3c4d16ba9d7f5a88c1b2688b56fcf3805
Peter Sirka <petersirka@gmail.com>
2020-03-23 16:07:05 +0100

    Fixed auto-close brackets 20 times.

--- public/js/func.js
+++ public/js/func.js
@@ -1,114 +1,86 @@
 var TIDYUPWHITE = new RegExp(String.fromCharCode(160), 'g');
 
-FUNC.tabscount = function(val) {
-	var count = 0;
-	for (var i = 0; i < val.length; i++) {
-		if (val.charAt(i) === '\t')
-			count++;
-		else
-			break;
-	}
-	return count;
-};
+(function() {
 
-FUNC.wrapbracket = function(cm, pos) {
-
-	var line = cm.getLine(pos.line);
-	if (!(/function|switch|else|with|if\s\(/).test(line))
-		return;
-
-	var tabs = FUNC.tabscount(line);
-	var lines = cm.lineCount();
-	var plus = '';
-	var nl;
-
-	if (line.indexOf('= function') !== -1)
-		plus = ';';
-	else if (line.indexOf(', function') !== -1 || line.indexOf('(function') !== -1)
-		plus = ');';
-
-	if (pos.line + 1 >= lines) {
-		// end of value
-		cm.replaceRange('\n' + ''.padLeft(tabs + 1, '\t') + '\n' + ''.padLeft(tabs, '\t') + '}' + plus, pos, null, '+input');
-		pos.line++;
-		pos.ch = tabs + 1;
-		cm.setCursor(pos);
-		return true;
-	}
-
-	if (plus) {
-		var lchar = line.substring(line.length - 2);
-
-		if (lchar !== ');') {
-			lchar = line.charAt(line.length - 1);
-			if (lchar !== ';' && lchar !== ')')
-				lchar = '';
+	var TABSCOUNT = function(val) {
+		var count = 0;
+		for (var i = 0; i < val.length; i++) {
+			if (val.charAt(i) === '\t')
+				count++;
+			else
+				break;
 		}
+		return count;
+	};
 
-		if (lchar) {
-			pos.ch = line.length - lchar.length;
-			var post = {};
-			post.line = pos.line;
-			post.ch = line.length;
-			cm.replaceRange('', pos, post, '+move');
-		}
-	}
+	var TABS = function(count) {
+		var str = '';
+		for (var i = 0; i < count; i++)
+			str += '\t';
+		return str;
+	};
 
-	var wassomething = false;
-	for (var i = pos.line + 1; i < lines; i++) {
 
-		var l = cm.getLine(i);
-		var tc = FUNC.tabscount(l);
+	FUNC.wrapbracket = function(cm, pos) {
 
-		if (tc <= tabs) {
+		var line = cm.getLine(pos.line);
+		if (!(/function|switch|else|with|if\s\(/).test(line))
+			return;
 
-			pos.line = i;
-			pos.ch = tc;
+		var tabs = TABSCOUNT(line);
+		var lines = cm.lineCount();
+		var plus = '';
+		var nl;
 
-			if (l) {
-				if (tc <= tabs) {
-					nl = cm.getLine(i + 1);
-					var an = (!tc && tabs) || (!tc && !tabs) ? ('\n' + ''.padLeft(tabs, '\t')) : '';
+		if (line.indexOf('= function') !== -1)
+			plus = ';';
+		else if (line.indexOf(', function') !== -1 || line.indexOf('(function') !== -1)
+			plus = ');';
 
-					pos.ch--;
-
-					if (!an && tc && tabs && tc === tabs){
-						an = '\n' + ''.padLeft(tabs, '\t');
-						cm.replaceRange((l.indexOf('else') === -1 ? (''.padLeft(tabs, '\t') + an + '}' + plus + '\n' + (nl || (l && l.indexOf('}') === -1) ? '\n' : '') + ''.padLeft(tc - 1, '\t')) : ('} ')), pos, null, '+input');
-					} else {
-						cm.replaceRange((l.indexOf('else') === -1 ? (''.padLeft(tabs + (an ? 1 : 0), '\t') + an + '}' + plus + '\n' + (nl || (l && l.indexOf('}') === -1) ? '\n' : '')) : ('} ')), pos, null, '+input');
-						if (!an)
-							pos.line--;
-					}
-
-				} else {
-					cm.replaceRange('}\n', pos, null, '+input');
-				}
-
-			} else {
-
-				if (wassomething) {
-					if (tc <= tabs) {
-						// pos.line--;
-						cm.replaceRange(''.padLeft(tabs , '\t') + '}' + plus + '\n', pos, null, '+input');
-					} else {
-						cm.replaceRange(''.padLeft(tabs , '\t') + '}' + plus + '\n', pos, null, '+input');
-						pos.line--;
-					}
-				} else {
-					nl = cm.getLine(i + 1);
-					cm.replaceRange(''.padLeft(tabs + 1, '\t') + '\n' + ''.padLeft(tabs, '\t') + '}' + plus + (nl ? '\n' : ''), pos, null, '+input');
-				}
-			}
-
+		if (pos.line + 1 >= lines) {
+			// end of value
+			cm.replaceRange('\n' + TABS(tabs + 1) + '\n' + TABS(tabs) + '}' + plus, pos, null, '+input');
+			pos.line++;
 			pos.ch = tabs + 1;
 			cm.setCursor(pos);
 			return true;
-		} else
-			wassomething = true;
-	}
+		}
 
-};
+		if (plus) {
+			var lchar = line.substring(line.length - 2);
+
+			if (lchar !== ');') {
+				lchar = line.charAt(line.length - 1);
+				if (lchar !== ';' && lchar !== ')')
+					lchar = '';
+			}
+
+			if (lchar) {
+				pos.ch = line.length - lchar.length;
+				var post = {};
+				post.line = pos.line;
+				post.ch = line.length;
+				cm.replaceRange('', pos, post, '+move');
+			}
+		}
+
+		for (var i = pos.line + 1; i < lines; i++) {
+
+			var cl = cm.getLine(i);
+			var tc = TABSCOUNT(cl);
+
+			if (tc <= tabs) {
+				var nl = cl && cl.indexOf('}') === -1 ? true : false;
+				pos.line = i - 1;
+				pos.ch = 10000;
+				cm.replaceRange('\n' + TABS(tabs) + '}' + plus + (nl ? '\n' : ''), pos, null, '+input');
+				pos.ch = tabs.length;
+				cm.setCursor(pos);
+				return true;
+			}
+		}
+	};
+})();
 
 FUNC.cleanpath = function(val) {
 	return val.replace(/\/{2,}/g, '/');
