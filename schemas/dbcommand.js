3f631afa5e5d262986441bac3f0f2659c0c3aa1c
Peter Sirka <petersirka@gmail.com>
2020-03-25 16:18:24 +0100

    Fixed limits for tables.

--- schemas/dbcommand.js
+++ schemas/dbcommand.js
@@ -49,7 +49,7 @@ NEWSCHEMA('DBCommand', function(schema) {
 		};
 
 		if (model.query[0] === '-')
-			model.query = 'SELECT CASE WHEN table_schema=\'public\' THEN \'\' ELSE table_schema || \'.\' END || "table_name" as name, CASE WHEN table_type=\'BASE TABLE\' THEN \'TABLE\' ELSE table_type END as type FROM information_schema.tables WHERE table_schema NOT IN (\'pg_catalog\', \'information_schema\') UNION ALL SELECT "routine_name" as name, routine_type as type FROM information_schema.routines WHERE routines.specific_schema=\'public\' LIMIT 20';
+			model.query = 'SELECT CASE WHEN table_schema=\'public\' THEN \'\' ELSE table_schema || \'.\' END || "table_name" as name, CASE WHEN table_type=\'BASE TABLE\' THEN \'TABLE\' ELSE table_type END as type FROM information_schema.tables WHERE table_schema NOT IN (\'pg_catalog\', \'information_schema\') UNION ALL SELECT "routine_name" as name, routine_type as type FROM information_schema.routines WHERE routines.specific_schema=\'public\' LIMIT 150';
 
 		client.connect(function(err) {
 
