cdcd0caab35be9617ca6ba1e6b54124e3c7dc315
Peter Širka <petersirka@gmail.com>
2020-04-09 14:48:21 +0200

    Updated jComponent + fixed privileges.

--- definitions/main.js
+++ definitions/main.js
@@ -193,10 +193,10 @@ ON('ready', function() {
 		PREF.set('token', '1234567890');
 
 	// Added permissions
-	Fs.chmod(PATH.databases(), 777, NOOP);
-	Fs.chmod(PATH.databases('branch_backup.sh'), 777, NOOP);
-	Fs.chmod(PATH.databases('branch_restore.sh'), 777, NOOP);
-	Fs.chmod(PATH.databases('source_backup.sh'), 777, NOOP);
+	Fs.chmod(PATH.databases(), 0o700, NOOP);
+	Fs.chmod(PATH.databases('branch_backup.sh'), 0o700, NOOP);
+	Fs.chmod(PATH.databases('branch_restore.sh'), 0o700, NOOP);
+	Fs.chmod(PATH.databases('source_backup.sh'), 0o700, NOOP);
 
 	if (PREF.name)
 		CONF.name = PREF.name;
