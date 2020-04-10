36a0caacd44532da8b30adc96612863ed9c96688
Peter Širka <petersirka@gmail.com>
2020-04-10 14:22:41 +0200

    Fixed removed users.

--- public/js/default.js
+++ public/js/default.js
@@ -100,13 +100,19 @@ Thelpers.particon = function(type) {
 };
 
 Thelpers.initials = function(value, coloronly) {
-	var index = value.indexOf('.');
-	var arr = value.substring(index + 1).replace(/\s{2,}/g, ' ').trim().split(' ');
-	var initials = (arr[0].substring(0, 1) + (arr[1] || '').substring(0, 1));
-	var sum = 0;
 
-	for (var i = 0; i < value.length; i++)
-		sum += value.charCodeAt(i);
+	if (value) {
+		var index = value.indexOf('.');
+		var arr = value.substring(index + 1).replace(/\s{2,}/g, ' ').trim().split(' ');
+		var initials = (arr[0].substring(0, 1) + (arr[1] || '').substring(0, 1));
+		var sum = 0;
+
+		for (var i = 0; i < value.length; i++)
+			sum += value.charCodeAt(i);
+	} else {
+		sum = 0;
+		value = initials = '--';
+	}
 
 	return coloronly ? TTIC[sum % value.length] : '<span class="initials" style="background-color:{1}" title="{2}">{0}</span>'.format(initials, TTIC[sum % value.length], value);
 };
