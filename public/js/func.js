810f7b7eeb40b63cc179b460b78570fd4b9f4361
Peter Širka <petersirka@gmail.com>
2020-09-17 16:05:20 +0200

    Fixed `CMD+B`.

--- public/js/func.js
+++ public/js/func.js
@@ -1255,7 +1255,7 @@ FUNC.livereload = function(filename) {
 		timeout = 2500;
 
 	code.data && code.data.url && code.data.url.length > 3 && W.WSLIVERELOAD && setTimeout2('livereload', function(filename) {
-		W.WSLIVERELOAD.send(code.data.url.replace(/^(https|http):\/\//g, '') + filename);
+		W.WSLIVERELOAD.send(code.data.url.replace(/^(https|http):\/\//g, '') + filename + ',' + user.id);
 	}, timeout, null, filename || '');
 };
 
