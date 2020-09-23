0c85ea2004479805f7ecb6da176565c53eb51020
Peter Širka <petersirka@gmail.com>
2020-09-23 09:51:39 +0200

    Improved project view.

--- public/js/default.js
+++ public/js/default.js
@@ -158,6 +158,16 @@ Thelpers.filesize = function(value, decimals, type) {
 	return value ? value.filesize(decimals, type) : '...';
 };
 
+Thelpers.color = function(value) {
+	var hash = HASH(value, true);
+	var color = '#';
+	for (var i = 0; i < 3; i++) {
+		var value = (hash >> (i * 8)) & 0xFF;
+		color += ('00' + value.toString(16)).substr(-2);
+	}
+	return color;
+};
+
 Number.prototype.filesize = function(decimals, type) {
 
 	if (typeof(decimals) === 'string') {
