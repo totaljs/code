9795ec28321ac6432be9df591d496215e910f161
petersirka <petersirka@gmail.com>
2025-03-26 14:27:44 +0100

    Improved autocomplete list.

--- public/js/editor.js
+++ public/js/editor.js
@@ -1836,9 +1836,10 @@ SNIPPETS.push({ type: 'js', search: 'PUBLISH', text: '<b>PUBLISH</b>', code: 'PU
 SNIPPETS.push({ type: 'js', search: 'SUBSCRIBE', text: '<b>SUBSCRIBE</b>', code: 'SUBSCRIBE(\'\', model, true);', ch: 12 });
 SNIPPETS.push({ type: 'js', search: 'CALL', text: '<b>CALL</b>', code: 'CALL(\' --> exec\').params({}).query({}).promise($);', ch: 7, priority: 1 });
 SNIPPETS.push({ type: 'js', search: 'API', text: '<b>API</b>', code: 'API(\'\', \'schema\', {}).controller($).promise($);', ch: 6, priority: 1 });
-SNIPPETS.push({ type: 'js', search: 'NEWACTION', text: '<b>NEWACTION</b>', code: 'NEWACTION(\'\', {\n\t{0}name: \'\',\n\t{0}input: \'\',\n\t{0}route: \'+API ?\',\n\t{0}query: \'\',\n\t{0}params: \'\',\n\t{0}permissions: \'admin\',\n\t{0}// output: \'success:Boolean\',\n\t{0}action: async function($, model) {\n\t{0}\n\t{0}}\n});', ch: 12 });
+SNIPPETS.push({ type: 'js', search: 'NEWACTION', text: '<b>NEWACTION</b>', code: 'NEWACTION(\'\', {\n\t{0}name: \'\',\n\t{0}input: \'\',\n\t{0}route: \'+API ?\',\n\t{0}query: \'\',\n\t{0}params: \'\',\n\t{0}permissions: \'admin\',\n\t{0}// output: \'success:Boolean\',\n\t{0}action: async function($, model) {\n\t{0}\n\t{0}}\n{0}});', ch: 12 });
 SNIPPETS.push({ type: 'js', search: 'NEWSCHEMA', text: '<b>NEWSCHEMA</b> (v5)', code: 'NEWSCHEMA(\'@{1}\', \'id,*email:Email\');', ch: 13 });
 SNIPPETS.push({ type: 'js', search: 'NEWSCHEMA', text: '<b>NEWSCHEMA</b> (v4)', code: 'NEWSCHEMA(\'{1}\', function(schema) {\n\t{0}schema.define(\'key\', String, true);\n{0}});', ch: 12 });
+SNIPPETS.push({ type: 'js', search: 'NEWTRANSFORM', text: '<b>NEWTRANSFORM</b>', code: 'NEWTRANSFORM(\'\', async function($, value) {\n\t{0}value.name = value.name.toUpperCase();\n\t{0}$.next();\n{0}});', ch: 15 });
 SNIPPETS.push({ type: 'js', search: 'NEWAPI', text: '<b>NEWAPI</b>', code: 'NEWAPI(\'\', function(opt, next) {\n\t{0}RESTBuilder.API(\'https://yourtotalapp.com/api/\', opt.schema, opt.data).strict().callback(next);\n{0}});', ch: 9 });
 SNIPPETS.push({ type: 'js', search: 'NEWJSONSCHEMA', text: '<b>NEWJSONSCHEMA</b>', code: 'NEWJSONSCHEMA(\'\', {});', ch: 16 });
 SNIPPETS.push({ type: 'js', search: 'NEWPUBLISH', text: '<b>NEWPUBLISH</b>', code: 'NEWPUBLISH(\'\', \'\');', ch: 13 });
